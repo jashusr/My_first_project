caf9a0fb6de655ce786d677cda813a0dfe1dcbdd
Jashwanth SR <73991335+jashusr@users.noreply.github.com>
2024-07-26 08:55:27 +0530

    project updated
    imported metirels and removed adventure_women

--- My_First_project/Assets/scripts/mathfMoveto.cs
+++ My_First_project/Assets/scripts/mathfMoveto.cs
@@ -1,10 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class mathfMoveto : MonoBehaviour
 {
-    float value;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,17 +14,22 @@ public class mathfMoveto : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (true) 
-        {
-            value = Mathf.MoveTowards(value, 0, Time.deltaTime);
-        }
-        else
-        {
-            value = Mathf.MoveTowards(value, 1, Time.deltaTime);
-        }
+        
     }
     private void FixedUpdate()
     {
         
+    }
+
+    private void Move(Vector2 input)
+    {
+        transform.Translate(new Vector3(input.x, input.y) * 10.0f * Time.deltaTime);
+    }
+
+    public void MoveInput(InputAction.CallbackContext context)
+    {
+        Vector2 moveInput = context.ReadValue<Vector2>();
+        Move(moveInput);
+
     }
 }
